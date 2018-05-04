934d05b36722a4b150e95a09a1f354f66f62ad33
VipeX64 <tomas.filipsgarage@gmail.com>
2018-05-04 14:38:17 +0200

    merge

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var database = [{question:"Jaroslav Seifer napsal dílo 'Na vlnách T.S.F'",answer:true}, {question:"Skupina Surrealismus v ČSR byla založena roku 1924",answer:false},{question:"Období proletářské poezie se datuje kolem let 1920-1924",answer:true}];
 //databáze všech otázek
 
@@ -12,15 +11,6 @@ h3.innerHTML = database[questionNumber].question;
 
 function correct(){ //funkce aktivovaná v případě, že uživatel odpoví ANO
     if(database[questionNumber].answer){
-=======
-var database = [{ question: "Jaroslav Seifer napsal dílo 'Na vlnách T.S.F'", answer: true }, { question: "Skupina Surrealismus v ČSR byla založena roku 1924", answer: false }, { question: "Období proletářské poezie se datuje kolem let 1920-1924", answer: true }];
-var questionNumber = Math.floor(Math.random() * database.length);
-console.log(questionNumber);
-var h3 = document.getElementById("qH3");
-h3.innerHTML = database[questionNumber].question;
-function correct() {
-    if (database[questionNumber].answer) {
->>>>>>> 1f6d4c72d6b211a53308876565ae8168dad3c75b
         //správná odpověď
         alert("Ano, správně");
     } else {
@@ -28,13 +18,8 @@ function correct() {
         alert("Kdepak");
     }
 }
-<<<<<<< HEAD
 function incorrect(){ // funkce aktivovaná v případě, že uživatel odpoví NE
     if(!database[questionNumber].answer){
-=======
-function incorrect() {
-    if (!database[questionNumber].answer) {
->>>>>>> 1f6d4c72d6b211a53308876565ae8168dad3c75b
         //správná odpověď
         alert("Ano, správně");
     } else {
