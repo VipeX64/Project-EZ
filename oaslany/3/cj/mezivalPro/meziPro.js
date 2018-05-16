39c3124ba662441d04a614394388eccc66acd61c
VipeX64 <netopyr.64@seznam.cz>
2018-05-16 04:07:21 +0200

    cj_almost_ready

--- oaslany/3/cj/mezivalPro/meziPro.js
+++ oaslany/3/cj/mezivalPro/meziPro.js
@@ -2,76 +2,164 @@ $("#mainCircuit").hide(); // vše je schované, dokud se vše potřebné nenačt
 var loadingScreen = $("#loadingScreen");
 var theme = "Meziválečná próza"; // otázky z tématu:
 var thQuestion = 1;
+var correctAudio = new Audio("../../../../packages/audio/RightSound.mp3");
+var wrongAudio = new Audio("../../../../packages/audio/WrongSound.mp3");
 
 var database = [
-{used:false, question: "Dlouhodobý majetek se rozděluje na finančí, oběžný a abstraktní.", answer: false, verdict: "Dlouhodobý majetek se dělí na hmotný, nehmotný a finanční." }, 
-{used:false, question: "Dle zákona o účetnictví musí být vstupní cena dlouhodobého hmotného majetku nad 40.000,- Kč.", answer: false, verdict: "Každá účetní jednotka si dle zákona o účetnictví může ve směrnici nastavit vstupní cenu svého majetku." }, 
-{used:false, question: "Účetní operace jako např: výměna oleje v autě, oprava rozbitých dveří nebo aplikace termální pasty na procesor se účtují na účet 511 - Opravy a udržování.", answer: true, verdict: "Nejedná se zde o technické zhodnocení, neměníme zde žádné parametry majetku, proto se zde jedná o opravy a údržbu." }, 
-{used:false, question: "Pořízení DDM účtujeme na účet 041 a 042.", answer: true, verdict: "Pořízení hmotného DM se účtuje na účet 042 a pořízení nehmotného DM na účet 041." }, 
-{used:false, question: "Každá účetní jednotka je povinna vést jak účetní tak daňové odepisování majetku.",answer:false,verdict:"Účetní jednotka je povinna vést daňové odepisování, účetní způsob odepisování slouží pouze pro ÚJ jakožto přehled o skutečném opotřebení majetku."},
-{used:false, question: "ÚJ je povinna odepisovat veškerý hmotný majetek, bez výjimek.",answer:false,verdict:"Pozemky a mistrovská díla nelze odepisovat, tudíž nemůžeme odepisovat všechen DHM."},
-{used:false, question: "ÚJ má v případě krádeže majetku právo na poloviční odpis",answer:true,verdict:"V případě dokázaného odcizení majetku má ÚJ právo na snížení základu daně o poloviční odpis."},
-{used:false, question: "V případě, že si DM vyrobí podnik sám, tak se zavedení majetku do provozu účtuje na účet 588 - Aktivace dlouhodobého hmotného majetku.",answer:true,verdict:"A v případě že si podnik sám naprogramuje software tak se účtuje na účet 587 - Aktivace dlouhodobého hmotného majetku."}
+    { question: "Karel Čapek a Josef Čapek se zasloužili o zmezinárodnění jakého slova?", a: "Kvído", b: "Motor", c: "Robot", d: "Televize", corr: 3, used:false },
+    { question: "Kde se narodil Karel Čapek a jakého povolání byla hlava rodiny?", a: "V Pardubicích, právník", b: "Ve Skalici, lékař", c: "V Benešově, vojenský generál",d:"V Malých Svatoňovicích v podhůří Krkonoš, lékař",corr:4, used:false},
+    { question:"Co je to sloupek?", a:"Krátký odstavec kritizující politickou činnost",b:"Modernější obdoba Nerudova fejetonu s vtipným zpracováním",c:"Obrázek složený z různých písmenek",d:"Menší podstavec navržený pro exponáty v muzeu",corr:2, used:false}
 ];
+
 //databáze všech otázek {question:"",answer:,verdict:""}
-$("#totalQuestions").append(database.length);
+$("#totalQuestions").append(database.length); // v modalu okně -> kolik je dohromady otázek v databázi
 $("#theme").append(theme); // Otázky z tématu: ......
-var questionNumber = Math.floor(Math.random() * database.length);// číslo náhodně zvolené otázky
+
 var questionNumber = Math.floor(Math.random() * database.length);// číslo náhodně zvolené otázky
 var ender = document.getElementById("end");
 var enderHelp = document.getElementById("endFade");
 var h3 = document.getElementById("qH3");
+
+function detectCorr(){
+    if(database[questionNumber].corr == 1){
+        return $("#a");
+    } else if(database[questionNumber].corr==2){
+        return $("#b");
+    } else if (database[questionNumber].corr ==3){
+        return $("#c");
+    } else {
+        return $("#d");
+    }
+}
+////////////////////////////////////////////////////////////////////////
+$("#a").html(database[questionNumber].a);
+if(database[questionNumber].a.length > 35){
+    $("#a").css("font-size","1.5em");
+}
+$("#b").html(database[questionNumber].b);
+if(database[questionNumber].b.length > 35){
+    $("#b").css("font-size","1.5em");
+}    /////////////////////////////////////////////// v případě moc dlouhé odpovědi zmenší písmo, aby se vešla do boxíku
+$("#c").html(database[questionNumber].c);
+if(database[questionNumber].c.length > 35){
+    $("#c").css("font-size","1.5em");
+}
+$("#d").html(database[questionNumber].d);
+if(database[questionNumber].d.length > 35){
+    $("#d").css("font-size","1.5em");
+}
+/////////////////////////////////////////////////////////////////////////
 $("#qNum").html(thQuestion);
 console.log(questionNumber); //zobrazení čísla otázky
 h3.innerHTML = database[questionNumber].question; //vloží do nadpisu znění otázky
 
-function rollRandomQ(){
+
+
+
+
+function rollRandomQ() {
     questionNumber = Math.floor(Math.random() * database.length);
 }
 
-function chooseQ(){
-    while(database[questionNumber].used){
+function chooseQ() {
+    while (database[questionNumber].used) {
         rollRandomQ();
     }
     h3.innerHTML = database[questionNumber].question; // vložení otázky do nadpisu
 }
-
-function correct() { //funkce aktivovaná v případě, že uživatel odpoví ANO
-    if (database[questionNumber].answer && thQuestion!=database.length) {
-        //správná odpověď
-        database[questionNumber].used = true;
-        chooseQ();
-        thQuestion = thQuestion+1;
-        $("#qNum").html(thQuestion);
-    } else if(!database[questionNumber].answer && thQuestion!=database.length) {
-        //špatná odpověď
-        database[questionNumber].used = true;
-        chooseQ();
-        thQuestion = thQuestion+1;
-        $("#qNum").html(thQuestion);
-    } else {
-        ender.style.display="block";
-        enderHelp.style.display="block";
-    }
-}
-function incorrect() { // funkce aktivovaná v případě, že uživatel odpoví NE
-    if (!database[questionNumber].answer && thQuestion!=database.length) {
-        //správná odpověď
-        database[questionNumber].used = true;
-        chooseQ();
-        thQuestion = thQuestion+1;
-        $("#qNum").html(thQuestion);
-    } else if (database[questionNumber].answer && thQuestion!=database.length) {
-        //špatná odpověď
-        database[questionNumber].used = true;
-        chooseQ();
-        thQuestion = thQuestion+1;
-        $("#qNum").html(thQuestion);
-    } else {
-        ender.style.display="block";
-        enderHelp.style.display="block";
-    }
-}
+///////////////////////////////////////////////////////////////////////////
+function a() {                                                           //
+    if(database[questionNumber].corr == 1){                              //
+        //turn green                                                     //
+        $("#a").css("color", "green");
+        correctAudio.play();                                   //
+    } else {                                                             //
+        // turn red and find correct answer -> green it                  //
+        $("#a").css("color", "red");
+        wrongAudio.play();                                     //        
+        detectCorr().css("color", "green");                              //
+                                                                         // 
+    }                                                                    //
+}                                                                        // 
+                                                                         //
+function b() {                                                           //
+    if(database[questionNumber].corr == 2){                              // 
+        //turn green                                                     //
+        $("#b").css("color", "green");
+        correctAudio.play();                                             //
+    } else {                                                             //
+        // turn red and find correct answer -> green it                  //
+        $("#b").css("color", "red");                                     //
+        detectCorr().css("color", "green");
+        wrongAudio.play();                                               //
+                                                                         //
+    }                                                                    // funkce zvýrazňující správné a špatné odpovědi
+}                                                                        // 
+                                                                         //                                               
+function c() {                                                           // 
+    if(database[questionNumber].corr == 3){                              //  
+        //turn green
+        correctAudio.play();                                             //       
+        $("#c").css("color", "green");                                   //                         
+    } else {                                                             //                             
+        // turn red and find correct answer -> green it                  //                                          
+        $("#c").css("color", "red");                                     //                                                     
+        detectCorr().css("color", "green");
+        wrongAudio.play();                                               //                              
+                                                                         //                 
+    }                                                                    //                      
+}                                                                        //                                                
+                                                                         //                 
+function d() {                                                           //                               
+    if(database[questionNumber].corr == 4){                              //                              
+        //turn green
+        correctAudio.play();                                             //                                     
+        $("#d").css("color", "green");                                   //                                                       
+    } else {                                                             //                             
+        // turn red and find correct answer -> green it                  //                                          
+        $("#d").css("color", "red");                                     //                       
+        detectCorr().css("color", "green");
+        wrongAudio.play();                                               //                              
+                                                                         //                 
+    }                                                                    //                      
+}                                                                        //                  
+///////////////////////////////////////////////////////////////////////////
+// function correct() { //funkce aktivovaná v případě, že uživatel odpoví ANO
+//     if (database[questionNumber].answer && thQuestion!=database.length) {
+//         //správná odpověď
+//         database[questionNumber].used = true;
+//         chooseQ();
+//         thQuestion = thQuestion+1;
+//         $("#qNum").html(thQuestion);
+//     } else if(!database[questionNumber].answer && thQuestion!=database.length) {
+//         //špatná odpověď
+//         database[questionNumber].used = true;
+//         chooseQ();
+//         thQuestion = thQuestion+1;
+//         $("#qNum").html(thQuestion);
+//     } else {
+//         ender.style.display="block";
+//         enderHelp.style.display="block";
+//     }
+// }
+// function incorrect() { // funkce aktivovaná v případě, že uživatel odpoví NE
+//     if (!database[questionNumber].answer && thQuestion!=database.length) {
+//         //správná odpověď
+//         database[questionNumber].used = true;
+//         chooseQ();
+//         thQuestion = thQuestion+1;
+//         $("#qNum").html(thQuestion);
+//     } else if (database[questionNumber].answer && thQuestion!=database.length) {
+//         //špatná odpověď
+//         database[questionNumber].used = true;
+//         chooseQ();
+//         thQuestion = thQuestion+1;
+//         $("#qNum").html(thQuestion);
+//     } else {
+//         ender.style.display="block";
+//         enderHelp.style.display="block";
+//     }
+// }
 
 
 // po úplném načtení platformy, se vše zobrazí
@@ -79,6 +167,6 @@ $(document).ready(function () {
     $("#mainCircuit").fadeIn(1500);
     console.log("yeis");
     loadingScreen.fadeOut();
-    
+
 
 })
