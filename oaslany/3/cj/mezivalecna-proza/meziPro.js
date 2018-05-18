e23a021ab49873310a1bcb99215707ba9e2f8ac9
VipeX64 <tomas.filipsgarage@gmail.com>
2018-05-18 15:08:37 +0200

    fixed

---
+++ oaslany/3/cj/mezivalecna-proza/meziPro.js
@@ -0,0 +1,30 @@
+$("#mainCircuit").hide(); // vše je schované, dokud se vše potřebné nenačte
+const loadingScreen = $("#loadingScreen");
+const theme = "Meziválečná próza"; // otázky z tématu:
+
+const database = [
+    {questionInput: "Karel Čapek a Josef Čapek se zasloužili o zmezinárodnění jakého slova?", answeranswerA: "Kvído", answerB: "Motor", answerC: "Robot", answerD: "Televize", correctRank: 3, used: false },
+    {questionInput: "Kde se narodil Karel Čapek a jakého povolání byla hlava rodiny?", answerA: "V Pardubicích, právník", answerB: "Ve Skalici, lékař", answerC: "V Benešově, vojenský generál", answerD: "V Malých Svatoňovicích v podhůří Krkonoš, lékař", correctRank: 4, used: false },
+    {questionInput: "Co je to sloupek?", answerA: "Krátký odstavec kritizující politickou činnost", answerB: "Modernější obdoba Nerudova fejetonu s vtipným zpracováním", answerC: "Obrázek složený z různých písmenek", answerD: "Menší podstavec navržený pro exponáty v muzeu", correctRank: 2, used: false },
+    {questionInput: "Ve kterém ze svých vývojových období napsal Karel Čapek sbírku sedmi povídek Zářivé hlubiny a čím byla stejnojmenná povídka inspirována?",answerA:"2. období, vynález knihtisku",answerB:"1. období, zkázou Titaniku",answerC:"4. období, smrtí svého bratra",answerD:"1. období, cestami po světě",correctRank:2,used:false},
+    {questionInput: "Co to byla Francouzská poezie nové doby?",answerA:"Překlady z tvorby moderních francouzských básníků",answerB:"Důkladné popisy bitev ve Versailles",answerC:"Krycí jméno pro bitvu u Waterloo",answerD:"Hnutí za zavedení nového uměleckého směru",correctRank:1,used:false},
+    {questionInput:"V jakých letech probíhalo 2. vývojové období Karla Čapka a co je po toto období typické?",answerA:"1911-1915, psaní povídek",answerB:"1899-1910, líčení příběhů z cest",answerC:"1921-1927, tvorba dramatických a románových utopií",answerD:"1930-1940, kritika tehdejšího politického režimu",correctRank:3,used:false},
+    {questionInput:"Které z těchto děl nepatří mezi dramata od Karla Čapka?",answerA:"Ze života hmyzu",answerB:"R. U. R.",answerC:"Věc Makropulos",answerD:"30 případů Majora Zemana",correctRank:4,used:false},
+    {questionInput:"Které z těchto děl patří mezi fantastsicko-utopické romány od Karla Čapka?",answerA:"Továrna na absolutno",answerB:"Tisíc a jedna noc",answerC:"Vítězná hvězda",answerD:"Bylo nás 5",correctRank:1,used:false},
+    {questionInput:"Který z těchto výroků není pravdivý o Karlu Čapkovi?",answerA:"Studoval gymnázium v Hradci Králové, v Praze, poté filozofii na UK v Praze",answerB:"Se svým bratrem Josefem podnikl studijní cestu do Paříže",answerC:"Působil jako novinář v Národních listech (1917-1921)",answerD:"Oslavoval technický pokrok, podporoval větší rozvoj technologické civilizace",correctRank:4,used:false},
+    {questionInput:"Který z těchto výroků není pravdivý o Karlu Čapkovi?",answerA:"Po mnichovské kapitulaci r. 1938 byl politicky napadán, odmítal emigrovat",answerB:"Onemocněl zápalem plic a umíra o Vánocích 1938",answerC:"Měl nenávistný vztah se svým bratrem",answerD:"V roce 1935 se oženil s herečkou Olgou Scheinpfflugovou",correctRank:3,used:false}
+];
+
+//databáze všech otázek {question:"",answer:,verdict:""}
+$("#totalQuestions").append(database.length); // v modalu okně -> kolik je dohromady otázek v databázi
+$("#theme").append(theme); // Otázky z tématu: ......
+
+// po úplném načtení platformy, se vše zobrazí
+$(document).ready(function () {
+    setTimeout(() => {
+        $("#mainCircuit").fadeIn(1500);
+        loadingScreen.fadeOut();
+        next();
+    }, 1000)
+
+})
--- packages/script/global.js
+++ packages/script/global.js
@@ -14,11 +14,11 @@ const enderHelp = document.getElementById("endFade");
 let h3 = document.getElementById("qH3");
 
 function detectCorr() {  ///////detekuje část elementu, podle korektní odpovědi
-    if (database[questionNumber].corr == 1) {
+    if (database[questionNumber].correctRank== 1) {
         return $("#a");
-    } else if (database[questionNumber].corr == 2) {
+    } else if (database[questionNumber].correctRank== 2) {
         return $("#b");
-    } else if (database[questionNumber].corr == 3) {
+    } else if (database[questionNumber].correctRank== 3) {
         return $("#c");
     } else {
         return $("#d");
@@ -33,7 +33,7 @@ function chooseQ() { // v případě, že byla otázky již použita, najdi jino
     while (database[questionNumber].used) {
         rollRandomQ();
     }
-    h3.innerHTML = database[questionNumber].question; // vložení otázky do nadpisu
+    h3.innerHTML = database[questionNumber].questionInput; // vložení otázky do nadpisu
 }
 
 function next() { //Další otázka
@@ -50,30 +50,30 @@ function next() { //Další otázka
         $(".yesOrNo").css("color", "black");
         canPlay = true;
         chooseQ();
-        $("#a").html(database[questionNumber].a);
-        if (database[questionNumber].a.length > 35) {
+        $("#a").html(database[questionNumber].answerA);
+        if (database[questionNumber].answerA.length > 35) {
             $("#a").css("font-size", "1.5em");
         }
-        $("#b").html(database[questionNumber].b);
-        if (database[questionNumber].b.length > 35) {
+        $("#b").html(database[questionNumber].answerB);
+        if (database[questionNumber].answerB.length > 35) {
             $("#b").css("font-size", "1.5em");
         }    ////////////// v případě moc dlouhé odpovědi zmenší písmo, aby se vešla do boxíku
-        $("#c").html(database[questionNumber].c);
-        if (database[questionNumber].c.length > 35) {
+        $("#c").html(database[questionNumber].answerC);
+        if (database[questionNumber].answerC.length > 35) {
             $("#c").css("font-size", "1.5em");
         }
-        $("#d").html(database[questionNumber].d);
-        if (database[questionNumber].d.length > 35) {
+        $("#d").html(database[questionNumber].answerD);
+        if (database[questionNumber].answerD.length > 35) {
             $("#d").css("font-size", "1.5em");
         }
-        h3.innerHTML = database[questionNumber].question; //vloží do nadpisu znění otázky
+        h3.innerHTML = database[questionNumber].questionInput; //vloží do nadpisu znění otázky
     }
 }
 
 function renderAns(x) {
     if (!canPlay) {
         console.log("nothing");
-    } else if (x == a && database[questionNumber].corr == 1) {
+    } else if (x == a && database[questionNumber].correctRank== 1) {
         detectCorr().css("color", "green");
         correctAudio.play();
         canPlay = false;
@@ -83,7 +83,7 @@ function renderAns(x) {
         correctAnswer += 1;
         console.log(thQuestion);
         console.log(correctAnswer + "points");
-    } else if (x == b && database[questionNumber].corr == 2) {
+    } else if (x == b && database[questionNumber].correctRank== 2) {
         detectCorr().css("color", "green");
         correctAudio.play();
         canPlay = false;
@@ -93,7 +93,7 @@ function renderAns(x) {
         correctAnswer += 1;
         console.log(thQuestion);
         console.log(correctAnswer + "points");
-    } else if (x == c && database[questionNumber].corr == 3) {
+    } else if (x == c && database[questionNumber].correctRank== 3) {
         detectCorr().css("color", "green");
         correctAudio.play();
         canPlay = false;
@@ -103,7 +103,7 @@ function renderAns(x) {
         correctAnswer += 1;
         console.log(thQuestion);
         console.log(correctAnswer + "points");
-    } else if (x == d && database[questionNumber].corr == 4) {
+    } else if (x == d && database[questionNumber].correctRank== 4) {
         detectCorr().css("color", "green");
         correctAudio.play();
         canPlay = false;
