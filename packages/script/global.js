9b18f8ebd028a81eabd450444141a010019d7d2d
VipeX64 <tomas.filipsgarage@gmail.com>
2018-05-23 17:37:45 +0200

    EZMAT V1.0

--- packages/script/global.js
+++ packages/script/global.js
@@ -3,9 +3,16 @@ let thQuestion = 0;
 let canPlay = true;
 let correctAnswer = 0;
 let canAppend = true;
+let startWidth = window.screen.availWidth;
+let mobile;
+if (startWidth < 800) {
+    mobile = true;
+} else if (startWidth >= 800) {
+    mobile = false;
+}
 
-const correctAudio = new Audio("../../../../packages/audio/RightSound.mp3");
-const wrongAudio = new Audio("../../../../packages/audio/WrongSound.mp3");
+const correctAudio = new Audio("../../../../../packages/audio/RightSound.mp3");
+const wrongAudio = new Audio("../../../../../packages/audio/WrongSound.mp3");
 
 //databáze všech otázek {question:"",a:"",b:"",c:",d:"",corr:,used:false}
 let questionNumber;
@@ -14,12 +21,26 @@ const ender = document.getElementById("end");
 const enderHelp = document.getElementById("endFade");
 let h3 = document.getElementById("qH3");
 
+function checkWidth() { // každou půlsekundu to zjistí, zda se nezměnila velikost stránky
+    let currentWidth = window.screen.availWidth;
+
+    if (currentWidth !== startWidth) {
+        startWidth = currentWidth;
+        mobile = currentWidth < 800 ? true : false;
+        correctFont();
+    }
+    setInterval(function () {
+        checkWidth();
+    }, 500);
+}
+checkWidth();
+
 function detectCorr() {  ///////detekuje část elementu, podle korektní odpovědi
-    if (database[questionNumber].correctRank== 1) {
+    if (database[questionNumber].correctRank == 1) {
         return $("#a");
-    } else if (database[questionNumber].correctRank== 2) {
+    } else if (database[questionNumber].correctRank == 2) {
         return $("#b");
-    } else if (database[questionNumber].correctRank== 3) {
+    } else if (database[questionNumber].correctRank == 3) {
         return $("#c");
     } else {
         return $("#d");
@@ -28,7 +49,7 @@ function detectCorr() {  ///////detekuje část elementu, podle korektní odpov�
 
 function rollRandomQ() {  //roll random otázky
     questionNumber = Math.floor(Math.random() * database.length);
-    database[questionNumber].used = (database[questionNumber].used === "false" || database[questionNumber].used === false) ? false : true;  
+    database[questionNumber].used = (database[questionNumber].used === "false" || database[questionNumber].used === false) ? false : true;
 }
 
 function chooseQ() { // v případě, že byla otázky již použita, najdi jinou +
@@ -52,69 +73,22 @@ function next() { //Další otázka
         $(".yesOrNo").css("color", "black");
         canPlay = true;
         chooseQ();
-        $("#a").html(database[questionNumber].answerA);
-        if (database[questionNumber].answerA.length > 35) {
-            $("#a").css("font-size", "1.5em");
-        }
-        $("#b").html(database[questionNumber].answerB);
-        if (database[questionNumber].answerB.length > 35) {
-            $("#b").css("font-size", "1.5em");
-        }    ////////////// v případě moc dlouhé odpovědi zmenší písmo, aby se vešla do boxíku
-        $("#c").html(database[questionNumber].answerC);
-        if (database[questionNumber].answerC.length > 35) {
-            $("#c").css("font-size", "1.5em");
-        }
-        $("#d").html(database[questionNumber].answerD);
-        if (database[questionNumber].answerD.length > 35) {
-            $("#d").css("font-size", "1.5em");
-        }
-        h3.innerHTML = database[questionNumber].questionInput; //vloží do nadpisu znění otázky
+        correctFont();
+        h3.innerHTML = database[questionNumber].questionInput;
     }
 }
 
 function renderAns(x) {
     if (!canPlay) {
         console.log("nothing");
-    } else if (x == a && database[questionNumber].correctRank== 1) {
-        detectCorr().css("color", "green");
-        correctAudio.play();
-        canPlay = false;
-        database[questionNumber].used = true;
-        $("#next").css("display", "block");
-        thQuestion += 1;
-        correctAnswer += 1;
-        console.log(thQuestion);
-        console.log(correctAnswer + "points");
-    } else if (x == b && database[questionNumber].correctRank== 2) {
-        detectCorr().css("color", "green");
-        correctAudio.play();
-        canPlay = false;
-        database[questionNumber].used = true;
-        $("#next").css("display", "block");
-        thQuestion += 1;
-        correctAnswer += 1;
-        console.log(thQuestion);
-        console.log(correctAnswer + "points");
-    } else if (x == c && database[questionNumber].correctRank== 3) {
-        detectCorr().css("color", "green");
-        correctAudio.play();
-        canPlay = false;
-        database[questionNumber].used = true;
-        $("#next").css("display", "block");
-        thQuestion += 1;
-        correctAnswer += 1;
-        console.log(thQuestion);
-        console.log(correctAnswer + "points");
-    } else if (x == d && database[questionNumber].correctRank== 4) {
-        detectCorr().css("color", "green");
-        correctAudio.play();
-        canPlay = false;
-        database[questionNumber].used = true;
-        $("#next").css("display", "block");
-        thQuestion += 1;
-        correctAnswer += 1;
-        console.log(thQuestion);
-        console.log(correctAnswer + "points");
+    } else if (x == a && database[questionNumber].correctRank == 1) {
+        scotWork();
+    } else if (x == b && database[questionNumber].correctRank == 2) {
+        scotWork();
+    } else if (x == c && database[questionNumber].correctRank == 3) {
+        scotWork();
+    } else if (x == d && database[questionNumber].correctRank == 4) {
+        scotWork();
     } else {
         if (x == a) {
             $("#a").css("color", "red");
@@ -137,4 +111,46 @@ function renderAns(x) {
 function renderPercentage() {
     var num = correctAnswer / thQuestion * 100;
     return num.toFixed(2);
+}
+
+function scotWork() {
+    detectCorr().css("color", "green");
+    correctAudio.play();
+    canPlay = false;
+    database[questionNumber].used = true;
+    $("#next").css("display", "block");
+    thQuestion += 1;
+    correctAnswer += 1;
+    console.log(thQuestion);
+    console.log(correctAnswer + "points");
+}
+function correctFont() {
+    console.log("it rendered");
+    console.log(mobile);
+    let currentBlock;
+    let answer;
+    let depends;
+    let shortie = database[questionNumber];
+    let variable = [{ currentBlock: "#a", currentAnswer: "answerA" }, { currentBlock: "#b", currentAnswer: "answerB" }, { currentBlock: "#c", currentAnswer: "answerC" }, { currentBlock: "#d", currentAnswer: "answerD" }];
+    for (let i = 0; i < 4; i++) {
+        currentBlock = $(variable[i].currentBlock);
+        answer = shortie[variable[i].currentAnswer];
+        currentBlock.html(answer);
+        if (answer.length > 50 && mobile) {
+            currentBlock.css("font-size", "2em");
+        } else if (answer.length > 50) {
+            currentBlock.css("font-size", "1.6em");
+        } else if (answer.length > 35 && mobile) {
+            currentBlock.css("font-size", "2.4em");
+        } else if (answer.length > 35) {
+            currentBlock.css("font-size", "2em");
+        } else if (answer.length <= 35 && mobile) {
+            currentBlock.css("font-size", "2.8em");
+        } else if (answer.length <= 35) {
+            currentBlock.css("font-size", "2em");
+        } else {
+            console.log("weird, but nobody came");
+        }
+
+    }
 }
